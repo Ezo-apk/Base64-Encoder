dcec8c529d15dc0204e6510e92431d001858db10
Dragos Isopencu <dragosisopencu@gmail.com>
2025-05-02 12:55:25 +0300

    Changed the drver code to use strtol, so as to no longer compare strings

--- main.c
+++ main.c
@@ -1,29 +1,41 @@
+#include <errno.h>
+
 #include "base64.h"
 
 int main() {
 
     char *ret;
     char input[MAX_INPUT_LENGTH];
-    char choice[100]; 
+    char choice_input[100];
+    short det;
 
+    
     // "S1JZUFRPTklTR1JFQQ=="
-
+    
     // KRYPTONISGREAT
     // S1JZUFRPTklTR1JFQVQ=
-
+    
 
     printf("Do you want to encode or decode a string?\nType 1 for encoding and 2 for decoding.\n");
-    if (!fgets(choice, 100, stdin)) {
+    if (!fgets(choice_input, 100, stdin)) {
         printf("Failed to get your choice, exitting\n");
         return 1;
     }
 
-    if (choice[0] == '1') {
-        printf("Chosen ENCODING, please write the string you want encoded:\n");
-    } else if (choice[0] == '2') {
-        printf("Chosen DENCODING, please write the string you want dencoded:\n");
-    } else {
-        printf("That is not a valid choice, run me again\n");
+    errno = 0;
+    det = (short)strtol(choice_input, NULL, 0);
+
+
+    switch (det) {
+        case 1:
+            printf("Chosen ENCODING, please write the string you want encoded:\n");
+            break;
+        case 2:
+            printf("Chosen DENCODING, please write the string you want dencoded:\n");
+            break;
+        default:
+            printf("That is not a valid choice, run me again\n");
+            return 0;
     }
 
     if (!fgets(input, MAX_INPUT_LENGTH, stdin)) {
@@ -31,14 +43,11 @@ int main() {
         return 1;
     }
 
-    if (choice[0] == '1') {
-        ret = encodeBase64(input);
-        printf("\nYour encoded string is:\n%s\n", ret);
-    } else if (choice[0] == '2') {
-        ret = decodeBase64(input);
-        printf("\nYour dencoded string is:\n%s\n", ret);
-    }
+    ret = (det == 1) ? encodeBase64(input) : decodeBase64(input);
 
+    printf ("\nYour result is:\n%s\n", ret);
+
+    free(ret);
     return 0;
 }
 
